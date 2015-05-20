2528d03c7541b7398a9779d8c4590adb81b8a6d3
FlorianKah <florian.kaeutner@outlook.de>
2015-05-20 16:35:13 +0200

    Bug fixes

---
+++ Resonator.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Example skin
+ *
+ * This is an example skin showcasing the best practices, a companion to the MediaWiki skinning
+ * guide available at <https://www.mediawiki.org/wiki/Manual:Skinning>.
+ *
+ * The code is released into public domain, which means you can freely copy it, modify and release
+ * as your own skin without providing attribution and with absolutely no restrictions. Remember to
+ * change the license information if you do not intend to provide your changes on the same terms.
+ *
+ * @file
+ * @ingroup Skins
+ * @author Florian K
+ * @license CC0 (public domain) <http://creativecommons.org/publicdomain/zero/1.0/>
+ */
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'This is an extension to the MediaWiki package and cannot be run standalone.' );
+}
+
+$wgExtensionCredits['skin'][] = array(
+		'path' => __FILE__,
+		'name' => 'Resonator', // name as shown under [[Special:Version]]
+		'namemsg' => 'skinname-resonator', // used since MW 1.24, see the section on "Localisation messages" below
+		'version' => '0.8 alpha',
+		'url' => 'https://www.mediawiki.org/',
+		'author' => '[https://mediawiki.org/ Florian K]',
+		'descriptionmsg' => 'resonator-desc', // see the section on "Localisation messages" below
+		'license' => 'GPL-2.0+'
+);
+
+$wgValidSkinNames['resonator'] = 'Resonator';
+
+$wgAutoloadClasses['SkinResonator'] = __DIR__ . '/Resonator.skin.php';
+$wgMessagesDirs['Resonator'] = __DIR__ . '/i18n';
+
+$wgDefaultUserOptions['cols'] = 134;	// Since the Navigation Bar is on top, we have much space to both sides and we need 135 colons for editing
+
+$wgResourceModules['skins.resonator.styles'] = array(
+	'styles' => array(
+		'Resonator/resources/screen.css'
+	),
+	'remoteBasePath' => &$GLOBALS['wgStylePath'],
+	'localBasePath' => &$GLOBALS['wgStyleDirectory'],
+);
+
+
---
+++ Resonator.skin.php
@@ -0,0 +1,212 @@
+<?php
+/**
+ * Skin file for the Example skin.
+ *
+ * @file
+ * @ingroup Skins
+ */
+
+/**
+ * SkinTemplate class for the Example skin
+ *
+ * @ingroup Skins
+ */
+class SkinResonator extends SkinTemplate {
+	public $skinname = 'Resonator', $stylename = 'Resonator',
+		$template = 'ResonatorTemplate', $useHeadElement = true;
+
+	/**
+	 * Add CSS via ResourceLoader
+	 *
+	 * @param $out OutputPage
+	 */
+	function setupSkinUserCss( OutputPage $out ) {
+		parent::setupSkinUserCss( $out );
+		$out->addModuleStyles( array(
+			'skins.resonator.styles'
+		) );
+
+		//Replace the following with your own google analytic info
+
+		$out->addHeadItem( 'analytics',
+				'<script type="text/javascript">'."
+		
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-0000000-00']);
+  _gaq.push(['_setDomainName', 'yourdomain.com/with-no-http://']);
+  _gaq.push(['_setAllowHash', 'false']);
+  _gaq.push(['_trackPageview']);
+		
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+		
+</script>"
+		);
+		$out->addHeadItem('bgimg', "<style>body {background-color: #102021;" .
+		"background: url('" . $GLOBALS['wgStylePath'] . "/Resonator/resources/bckgrnd.png');}</style>");
+	
+	}
+}
+
+/**
+ * BaseTemplate class for the Example skin
+ *
+			'mediawiki.skinning.interface',
+			'mediawiki.skinning.content.externallinks',
+ * @ingroup Skins
+ */
+class ResonatorTemplate extends BaseTemplate {
+	/**
+	 * Outputs a single sidebar portlet of any kind.
+	 */
+	private function outputPortlet( $box ) {
+		if ( !$box['content'] ) {
+			return;
+		}
+
+		?>
+		<div
+			role="navigation"
+			class="mw-portlet"
+			id="<?php echo Sanitizer::escapeId( $box['id'] ) ?>"
+			<?php echo Linker::tooltip( $box['id'] ) ?>
+		>
+			<h3><a href="">
+				<?php
+				if ( isset( $box['headerMessage'] ) ) {
+					$this->msg( $box['headerMessage'] );
+				} else {
+					echo htmlspecialchars( $box['header'] );
+				}
+				?></a>
+			</h3>
+
+			<?php
+			if ( is_array( $box['content'] ) ) {
+				echo '<ul>';
+				foreach ( $box['content'] as $key => $item ) {
+					echo $this->makeListItem( $key, $item );
+				}
+				echo '</ul>';
+			} else {
+				echo $box['content'];
+			}?>
+		</div>
+		<?php
+	}
+
+	/**
+	 * Outputs the entire contents of the page
+	 */
+	public function execute() {
+		
+		$this->html( 'headelement' ) ?>
+		<div id="mw-wrapper">
+			<a
+				id="p-logo"
+				role="banner"
+				href="<?php echo htmlspecialchars( $this->data['nav_urls']['mainpage']['href'] ) ?>"
+				<?php echo Xml::expandAttributes( Linker::tooltipAndAccesskeyAttribs( 'p-logo' ) ) ?>
+			>
+				<img
+					src="<?php $this->text( 'logopath' ) ?>"
+					alt="<?php $this->text( 'sitename' ) ?>"
+				/>
+			</a>
+
+
+			<div class="mw-body" role="main">
+				<?php if ( $this->data['sitenotice'] ) { ?>
+					<div id="siteNotice"><?php $this->html( 'sitenotice' ) ?></div>
+				<?php } ?>
+
+				<?php if ( $this->data['newtalk'] ) { ?>
+					<div class="usermessage"><?php $this->html( 'newtalk' ) ?></div>
+				<?php } ?>
+
+				<h1 class="firstHeading">
+					<span dir="auto"><?php $this->html( 'title' ) ?></span>
+				</h1>
+
+				<div id="siteSub"><?php $this->msg( 'tagline' ) ?></div>
+
+				<div class="mw-body-content">
+					<div id="contentSub">
+						<?php if ( $this->data['subtitle'] ) { ?>
+							<p><?php $this->html( 'subtitle' ) ?></p>
+						<?php } ?>
+						<?php if ( $this->data['undelete'] ) { ?>
+							<p><?php $this->html( 'undelete' ) ?></p>
+						<?php } ?>
+					</div>
+
+					<?php $this->html( 'bodytext' ) ?>
+
+					<?php $this->html( 'catlinks' ) ?>
+
+					<?php $this->html( 'dataAfterContent' ); ?>
+
+				</div>
+			</div>
+
+
+			<div id="mw-navigation">
+				<h2><?php $this->msg( 'navigation-heading' ) ?></h2>
+
+				<form
+					action="<?php $this->text( 'wgScript' ) ?>"
+					role="search"
+					class="mw-portlet"
+					id="p-search"
+				>
+					<input type="hidden" name="title" value="<?php $this->text( 'searchtitle' ) ?>" />
+
+					<h3><label for="searchInput"><?php $this->msg( 'search' ) ?></label></h3>
+
+					<?php echo $this->makeSearchInput( array( "id" => "searchInput", "size" => "35" ) ) ?>
+					<?php echo $this->makeSearchButton( 'go' ) ?>
+
+				</form>
+
+				<?php
+
+				$this->outputPortlet( array(
+					'id' => 'p-personal',
+					'headerMessage' => 'res-personal',
+					'content' => $this->getPersonalTools(),
+				) );
+
+				$this->outputPortlet( array(
+					'id' => 'p-namespaces',
+					'headerMessage' => 'res-namespace',
+					'content' =>$this->data['content_actions']
+				) );
+				
+				foreach ( $this->getSidebar() as $boxName => $box ) {
+					$this->outputPortlet( $box );
+				}
+
+				?>
+			</div>
+
+			<div id="mw-footer">
+				<?php foreach ( $this->getFooterLinks() as $category => $links ) { ?>
+					<ul role="contentinfo">
+						<?php foreach ( $links as $key ) { ?>
+							<li><?php $this->html( $key ) ?></li>
+						<?php } ?>
+					</ul>
+				<?php } ?>
+
+			</div>
+		</div>
+
+		<?php $this->printTrail() ?>
+		</body></html>
+
+		<?php
+	}
+}
